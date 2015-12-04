3ab7f9e753b3695562c0ad5153e73d71c17af62c
Albert Yang <ayoungbest@gmail.com>
2015-12-03 22:09:44 -0500

    remove implementation success

--- trees/AVL.cpp
+++ trees/AVL.cpp
@@ -2,6 +2,7 @@
 #include <set>
 #include <vector>
 #include <cmath>
+#include <random>
 
 using namespace std;
 
@@ -72,11 +73,46 @@ node insert(node root, int k) {
   return balance(root);
 }
 
+node get_min(node root) {
+  if (!root) return nullptr;
+  return root->left ? get_min(root->left) : root;
+}
+
+node remove_min(node root) {
+ if (!root) return nullptr;
+ if (!root->left) {
+  return root->right;
+ }
+ root->left = remove_min(root->left);
+ return balance(root);
+}
+
 bool contains(node root, int k) {
   if (!root) return false;
   if (root->key == k) return true;
   return k < root->key? contains(root->left, k) : contains(root->right, k);
 }
+
+node remove(node root, int k) {
+//  if (!contains(root, k)) return nullptr;
+  if (!root) return nullptr;
+  if (k < root->key) {
+    root->left = remove(root->left, k);
+  } else if (k > root->key) {
+    root->right = remove(root->right, k);
+  } else {
+    node l = root->left;
+    node r = root->right;
+    delete root;
+    if (!r) return l;
+    node min = get_min(r);
+    min->right = remove_min(root->right);
+    min->left = l;
+    return balance(min);
+  }
+  return balance(root);
+}
+
 
 bool is_balanced(node root) {
   if (!root) return true;
@@ -86,7 +122,12 @@ bool is_balanced(node root) {
   return true;
 }
 
-bool insert_test() {
+bool is_bst(node root) {
+  
+
+}
+
+bool test() {
   node root = nullptr;
   set<int> nodes;
   for (int i = 1; i <= 100; i++) {
@@ -101,21 +142,57 @@ bool insert_test() {
     bool in_the_set = (nodes.find(i) != nodes.end());
     if (in_the_tree != in_the_set) return false;
   }
+  if (!is_balanced(root)) {
+    cout << "The tree is imbalanced!" << endl;
+    return false;
+  } else {
+    cout << "Insert test success!" << endl;
+  }
+
+  for (int i = 0; i < 100000; i++) {
+    if (!contains(root, i)) {
+      nodes.insert(i);
+      root = insert(root, i);
+    }
+  }
+
+  for (int i = 0; i < 100000; i++) {
+    if (i % 3 == 0) {
+      nodes.erase(i);
+      root = remove(root, i);
+    }
+  }
+  for (int i = 0; i < 100000; i++) {
+    bool in_the_tree = contains(root, i);
+    bool in_the_set = (nodes.find(i) != nodes.end());
+    if (in_the_tree != in_the_set) {
+      cout << "Remove fail!" << endl;
+      return false;
+    }
+  }
 
   if (!is_balanced(root)) {
     cout << "The tree is imbalanced!" << endl;
     return false;
+  } else {
+    cout << "Remove test success!" << endl;
   }
 
+  if (!is_bst(root)) {
+    cout << "The tree is not a binary search tree!" << endl;
+    return false;
+  } else {
+    cout << "Binary Search Tree check pass!" << endl;
+  }
 
   return true;
 }
 
 int main() {
-  if (!insert_test()) 
-    cout << "Insert fail!" << endl;
+  if (!test()) 
+    cout << "Fail!" << endl;
   else 
-    cout << "Insert all test cases passed!" << endl;
+    cout << "All test cases passed!" << endl;
   return 0;
 }
 
